d295a4a63efb9efc9dfcb3b2de66337f881ef78b
profa4oka <profa4oka98@gmail.com>
2025-08-06 22:34:43 +0300

    Learn do with strings #2

--- MyPlayground.playground/Pages/Lesson5DoWithStrings.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Lesson5DoWithStrings.xcplaygroundpage/Contents.swift
@@ -1,7 +1,35 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+var greeting = "Do with string"
 
-//: [Next](@next)
+// MARK: String
+
+let name = "Tim"
+let username = "Cook"
+
+// MARK: Concantenation
+
+var fullName = name + " " + username
+var hello = "Hello, my name is "
+hello += fullName
+
+print(fullName)
+
+// MARK: Interpolation
+
+fullName = "Bob Johnson"
+var information = "\(hello) \(fullName)"
+
+let a = 5
+let b = 10
+let infoAboutSum = "The sum of a and b is \(a + b)"
+
+let multithreadString =
+"""
+ some text
+  some text
+   some text
+"""
+
+print(multithreadString)
+
